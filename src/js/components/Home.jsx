41ff7d5f494df2fc7ddcda64d4798564e14d6fa1
VicenteAGT <155429830+VicenteAGT@users.noreply.github.com>
2025-11-08 00:45:05 +0000

    [Updates by vicenteagt]

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,23 +4,19 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid fs-4 ">
+			<div className="row m-auto mt-4">
+				<div className="col-2 d-flex flex-row-reverse bg-black justify-content-center ">
+            <p className="me-2 text-white">{props.digitOne}</p>
+			<p className="me-2 text-white">{props.digitTwo}</p>
+			<p className="me-2 text-white">{props.digitThree}</p>
+			<p className="me-2 text-white">{props.digitFour}</p>
+			<p className="me-2 text-white">{props.digitFive}</p>
+			<p className="me-2 text-white">{props.digitSix}</p>
+			</div>
+			</div>
 		</div>
 	);
 };
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,8 +11,36 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+
+
+
+let counter = 0
+
+setInterval(() => {
+  let firstDigit = Math.floor((counter / 1) % 60);
+  let secondDigit = Math.floor((counter / 60) % 10);
+  let threeDigit = Math.floor((counter / 60) % 10);
+  let fourDigit = Math.floor((counter / 1000) % 10);
+  let fiveDigit = Math.floor((counter / 10000) % 10);
+  let sixDigit = Math.floor((counter / 100000) % 10);
+  counter++
+
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+     
+     
+      <Home
+        digitOne={firstDigit}
+        digitTwo={secondDigit}
+        digitThree={threeDigit}
+        digitFour={fourDigit}
+        digitFive={fiveDigit}
+        digitSix={sixDigit}
+      />
+     
+     
+    </React.StrictMode>,
+  )
+}, 1000)
+
